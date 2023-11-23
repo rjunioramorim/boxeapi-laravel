0179b767f505bcc46f8b33a1d022e41ba0bec984
Raimundo Junior Ribeiro de Amorim <rjunior_amorim@hotmail.com>
2023-11-23 15:19:22 -0300

    update controller list schedules

--- app/Http/Controllers/Api/Schedules/ListSchedulesController.php
+++ app/Http/Controllers/Api/Schedules/ListSchedulesController.php
@@ -18,45 +18,9 @@ class ListSchedulesController extends Controller
     {
         $this->service = $service;
     }
+
     public function __invoke(Request $request)
     {
-     
-
-        return ['data' => $this->service->listSchedules($request->day)];
-
-
-        // $date = $request->day ? Carbon::createFromFormat('Y-m-d', $request->day) : now();
-
-        // $request['isToday'] = $date->isToday();
-        // $request['hour'] = $date->addMinutes(10)->format('H:i');
-        // $request['day'] = $date->format('Y-m-d');
-
-        // $dayOfWeek = $date->dayOfWeek;
-
-        // if ($date->isPast()) {
-        //     return response()->json(['data' => []]);
-        // }
-        
-        // $schedules = Schedule::with(['checkins'], function ($query) use ($date) {
-        //     return $query->where('checkins.status', '!=', ScheduleType::CANCELED->value)->where('checkins.checkin_date', $date->format('Y-m-d'));
-        // })->where('day_of_week', $dayOfWeek)->get();
-
-        
-        // $clientId = auth()->user()->client->id;
-        // $schedules->each(function ($schedule) use ($clientId) {
-
-        //    $schedule->status = $schedule->checkins->where('client_id', $clientId)->pluck('status')->last();
-        // //    $schedule->status = $result != null ? ScheduleType::CONFIRMED->value : ScheduleType::SCHEDULED->value;
-        // });
-        
-        // $isEvent = $schedules->whereNotNull('event_date');
-
-        // if ($isEvent->count() > 0) {
-        //     return ListScheduleResource::collection($isEvent);
-        // }
-
-        // $schedules = $schedules->whereNull('event_date')->all();
-
-        // return ListScheduleResource::collection($schedules);
+        return response()->json(['data' => $this->service->listSchedules($request->day)]);
     }
 }
