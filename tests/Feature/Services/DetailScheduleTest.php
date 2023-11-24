892965c65258c5780f44c5ff1fafb63755995f07
Raimundo Amorim <rjunior_amorim@hotmail.com>
2023-11-23 21:32:36 -0300

    test list schedules

--- tests/Feature/Services/DetailScheduleTest.php
+++ tests/Feature/Services/DetailScheduleTest.php
@@ -12,30 +12,16 @@ afterEach(function () {
 test('Deve listar os dados do agendamento e os checkins relacionados a ele', function () {
     Carbon::setTestNow(Carbon::create(2023, 9, 11, 10, 15));
     $user = login();
-    
+
     $schedule =  Schedule::factory()
-            ->has(Checkin::factory(['hour' => '18:00', 'client_id' => $user->client->id]))
-            ->has(Checkin::factory(['hour' => '18:00']))
-            ->create(['hour' => '18:00', 'day_of_week' => 1]);
+        ->has(Checkin::factory(['hour' => '18:00', 'client_id' => $user->client->id]))
+        ->has(Checkin::factory(['hour' => '18:00']))
+        ->create(['hour' => '18:00', 'day_of_week' => 1]);
 
     $service = new SchedulesService();
 
     $schedule = $service->getSchedule($schedule, '2023-09-11');
-    
-    $response = [
-            "id" => 1,
-            "day" => "2023-09-11",
-            "hour" => "18:00",
-            "description" => null,
-            "professor" => "Prof: India",
-            "limit" => 12,
-            "vacancies" => 10,
-            "clients" => [
-                ["id" => 1, "status" => "scheduled", "isOwner" => true, "name" => $schedule[0]->checkins[0]->client['name'], 'avatar_url' => url($schedule[0]->checkins[0]->client['avatar_url'])],
-                ["id" => 2, "status" => "scheduled", "isOwner" => false, "name" => $schedule[0]->checkins[1]->client['name'], 'avatar_url' => url($schedule[0]->checkins[1]->client['avatar_url'])],
-            ],
 
-    ];
-
- expect($schedule)->toBeArray()->toMatchArray($response);
+    expect($schedule->id)->toBe(1);
+    expect($schedule->checkins)->toHaveCount(2);
 });
--- tests/Feature/Services/ListSchedulesServiceTest.php
+++ tests/Feature/Services/ListSchedulesServiceTest.php
@@ -19,68 +19,28 @@ afterEach(function () {
 test('Deve listar os horários do dia sem receber a data', function () {
     $service = new SchedulesService();
 
-    $schedules = $service->listSchedules();
-
-    $response = [
-           [
-            "id" => 3,
-            "day" => "2023-09-11",
-            "hour" => "17:00",
-            "description" => null,
-            "professor" => "Prof: India",
-            "limit" => 12,
-            "vacancies" => 12,
-            "userScheduled" => false,
-           ],
-          [
-            "id" => 1,
-            "day" => "2023-09-11",
-            "hour" => "18:00",
-            "description" => null,
-            "professor" => "Prof: India",
-            "limit" => 12,
-            "vacancies" => 10,
-            "userScheduled" => true,
-          ]
-    ];
-    expect($schedules)->toBeArray()
-        ->toMatchArray($response);
+    $schedules = $service->listSchedules(now()->format('Y-m-d'));
+    expect($schedules)->toHaveCount(2);
+    expect($schedules[0]->checkins)->toBeEmpty();
+    expect($schedules[1]->checkins)->toHaveCount(2);
 });
 
 
 test('Deve listar os horários do dia recebendo a data', function () {
     $service = new SchedulesService();
-    Carbon::setTestNow(Carbon::create(2023, 9, 11, 04, 15));    
+    Carbon::setTestNow(Carbon::create(2023, 9, 11, 04, 15));
     $schedules = $service->listSchedules('2023-09-12');
-    $response = [[
-            "id" => 5,
-            "day" => "2023-09-12",
-            "hour" => "05:00",
-            "description" => null,
-            "professor" => "Prof: India",
-            "limit" => 12,
-            "vacancies" => 12,
-            "userScheduled" => false,
-           ],
-          [
-            "id" => 6,
-            "day" => "2023-09-12",
-            "hour" => "19:00",
-            "description" => null,
-            "professor" => "Prof: India",
-            "limit" => 12,
-            "vacancies" => 12,
-            "userScheduled" => false,
-          ]
-        ];
-    expect($schedules)->toBeArray()->toMatchArray($response);
+
+    // dd($schedules->toArray());
+    expect($schedules)->toHaveCount(2);
+    expect($schedules[0]->checkins)->toBeEmpty();
+    expect($schedules[1]->checkins)->toHaveCount(0);
 });
 
 
-test('Deve retornar os  data informada for anterior ao dia atual', function () {
+test('Deve retornar array vazio se data informada for anterior ao dia atual', function () {
     $service = new SchedulesService();
 
-    
     $schedules = $service->listSchedules('2023-09-10');
 
     $response = [];
