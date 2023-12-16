5ef531729766ee477acd2d4bf074f31c8a94c449
Raimundo Amorim <rjunior_amorim@hotmail.com>
2023-12-15 21:54:35 -0300

    update path

--- app/Http/Controllers/Api/Auth/UpdateProfileController.php
+++ app/Http/Controllers/Api/Auth/UpdateProfileController.php
@@ -19,21 +19,21 @@ class UpdateProfileController extends Controller
     public function __invoke(Request $request)
     {
         $avatarUrl = auth()->user()->avatar_url;
-        
+
         if ($avatarUrl != null || $avatarUrl != 'images/avatar_default.jpg') {
-            Storage::delete('public/avatars/' . auth()->user()->avatar_url);
+            Storage::delete('public/' . auth()->user()->avatar_url);
         }
-        
+
         if ($request->hasFile('avatar_url')) {
             $image = $request->file('avatar_url');
-    
+
             $fileName = uniqid('image_') . '.' . $image->getClientOriginalExtension();
 
-             // Salva o arquivo no storage
-            $image->storeAs('public/avatars', $fileName);
-            
+            // Salva o arquivo no storage
+            $image->storeAs('public', $fileName);
+
             // Pode retornar o caminho do arquivo salvo se necessário
-            $user = User::where('id', auth()->user()->id)->first();         
+            $user = User::where('id', auth()->user()->id)->first();
             $user->update(['avatar_url' => $fileName]);
 
             return new ProfileResource($user);
